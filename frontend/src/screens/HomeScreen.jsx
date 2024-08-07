50ee426853cb81d9114f3d29190b0a07fe84244c
sadats90 <sadatshahriar.ss@gmail.com>
2024-08-07 07:01:02 +0600

    frontend initial

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Row,Col} from 'react-bootstrap'
+import products from '../products'
+import Products from '../components/Products'
+
+const HomeScreen = () => {
+  
+  return (
+   <>
+   <h1>Lates Products</h1>
+   <Row>
+    {products.map((product)=>(
+      
+        <Col sm={12} md={6} lg={4} xl={3}>
+           <Products product={product}/>
+        </Col>
+    ))}
+   </Row>
+   </>
+  )
+}
+
+export default HomeScreen
